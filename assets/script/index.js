c2153cc75e4d794820db40cb5ac1d190c49f7c0f
Guilherme-Calheiros <guilhermecalheiros522@gmail.com>
2024-05-01 15:45:41 -0300

    feat: finishing the landing page

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,4 +1,52 @@
+const accordion_item = document.querySelectorAll('.accordion-item');
+
+const slider = document.querySelector('.slider');
+const firstSlideWidth = slider.offsetWidth;
+
 function showMenu(){
     let mobileMenu = document.querySelector('.mobile-menu');
     mobileMenu.classList.toggle("show")
+}
+
+function slideLeft(){
+    slider.scrollLeft += -firstSlideWidth/3;
+    if (slider.scrollLeft <= 0) {
+        slider.scrollLeft = slider.scrollWidth;
+    }
+}
+
+function slideRight(){
+    slider.scrollLeft += firstSlideWidth/3;
+    if (slider.scrollLeft >= slider.scrollWidth - slider.clientWidth) {
+        slider.scrollLeft = 0;
+    }
+}
+
+accordion_item.forEach(item => {
+    const accordion_header = item.querySelector('.accordion-header');
+    
+    accordion_header.addEventListener("click", () => {
+        const accordion_content_item = item.querySelector('.accordion-content');
+
+        const content_active = document.querySelector('.active');
+
+        checkActive(item, accordion_content_item, content_active);
+    })
+})
+
+function checkActive(item, content, content_active){
+    const icon = item.querySelector('.material-symbols-outlined')
+
+    if(content_active){
+        content_active.style.height = '0px';
+        content_active.classList.remove('active');
+        icon.innerHTML = 'expand_more'
+    }
+
+    if(content !== content_active){
+        content.style.height = content.scrollHeight + 10 + 'px';
+        content.classList.add('active');
+        icon.innerHTML = 'expand_less'
+    }
+
 }
